d0fa7548dbe01fb71f57b0e245e1e5b6ea5b3f86
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-01-16 00:22:06 +0530

    installed necesssay dependencies and configured config files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App
