66934eebb91a8644e9485a23d74e0e7418518fbc
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-03-03 02:21:40 +0530

    minor changes

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -11,7 +11,7 @@ function Home() {
     const { isSignedIn } = useUser();
     const { theme, setTheme } = useContext(ThemeContext);
     return (
-        <div className={(theme === 'light') ? 'bg-gradient-to-r from-red-200 to-yellow-200' : 'bg-[url(home3.png)] bg-cover text-white'}>
+        <div className={(theme === 'light') ? 'bg-gradient-to-r from-red-200 to-yellow-200' : 'bg-[url("../home3.png")] bg-cover text-white'}>
             <Header />
             <div className={(theme === 'light') ? 'bg-[url("../textures/batthern.png")]' : ''}>
                 <div className='h-screen flex justify-center items-center'>
