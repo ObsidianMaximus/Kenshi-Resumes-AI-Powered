ff3d89a0aa33599337acc8ce1bc4b8cdba73007a
Abhishek Mathur <abhishekmathurofficial@gmail.com>
2025-03-04 09:12:18 +0530

    edited logo

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -14,9 +14,9 @@ function Header({ state }) {
 
     return (
         <div className={(theme === 'light') ? 'bg-gradient-to-r from-red-200 to-yellow-200 relative' : 'bg-[#000] relative'}>
-            <div className={(theme === 'light') ? 'bg-[url("../../textures/subtle-zebra-3d.png")] p-3 px-5 flex justify-between shadow-md' : 'p-3 px-5 flex justify-between shadow-lg shadow-[rgba(0,191,255,0.8)]'}>
+            <div className={(theme === 'light') ? 'bg-[url("../../textures/subtle-zebra-3d.png")] p-3 px-5 flex justify-between items-center shadow-md' : 'p-3 px-5 flex justify-between items-center shadow-lg shadow-[rgba(0,191,255,0.8)]'}>
                 <Link to={'/'}>
-                    <img src="../../../logo2.svg" alt="logo" width={100} height={100} />
+                    <img src={(theme === 'light') ? "../../../logo2.png" : "../../../logo.png"} alt="logo" width={180} height={100} />
                 </Link>
                 {
                     isSignedIn ?
