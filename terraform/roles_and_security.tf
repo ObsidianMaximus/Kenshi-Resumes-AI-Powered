6fd789813fa20ade7ef02d001f6c3242cb2071a9
Obsidian Maximus <obsidianmaximus@pm.me>
2025-07-22 22:44:29 +0530

    Allow 443

--- terraform/roles_and_security.tf
+++ terraform/roles_and_security.tf
@@ -47,6 +47,12 @@ resource "aws_security_group" "allow_http_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
